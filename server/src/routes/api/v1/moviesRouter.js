30d4e5e64e6f094bfcf4bf0aaac865db17d3367b
EJ Chamorro <102050774+ejchamo@users.noreply.github.com>
2023-10-31 17:09:01 -0400

    Update moviesRouter.js
    remove console log

--- server/src/routes/api/v1/moviesRouter.js
+++ server/src/routes/api/v1/moviesRouter.js
@@ -40,7 +40,6 @@ moviesRouter.post("/", async (req, res) => {
     if (error instanceof ValidationError) {
       return res.status(422).json({ errors: error.data });
     }
-    console.log(error);
     return res.status(500).json({ errors: error });
   }
 });
