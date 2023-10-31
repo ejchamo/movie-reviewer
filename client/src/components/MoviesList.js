26c4f3405f5a9caecaa5f80979f12c935a592972
Sky <43055693+skylfw@users.noreply.github.com>
2023-10-31 14:01:56 -0400

    create link for individual movie in movie index

--- client/src/components/MoviesList.js
+++ client/src/components/MoviesList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const MoviesList = (props) => {
   const [movies, setMovies] = useState([]);
@@ -23,7 +24,11 @@ const MoviesList = (props) => {
   }, []);
 
   const moviesListItems = movies.map((movieItem) => {
-    return <li key={movieItem.id}>{movieItem.title}</li>;
+    return (
+      <li key={movieItem.id}>
+        <Link to={`/movies/${movieItem.id}`}>{movieItem.title}</Link>
+      </li>
+    );
   });
 
   return (
