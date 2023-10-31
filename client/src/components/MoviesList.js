4645b87db0933dbd16009d62d40aa892e57bfd82
Sky <43055693+skylfw@users.noreply.github.com>
2023-10-31 10:54:00 -0400

    create react movie list component

---
+++ client/src/components/MoviesList.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+
+const MoviesList = (props) => {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const response = await fetch(`/api/v1/movies`);
+      if (!response.ok) {
+        const errorMessage = `${response.status} (${response.statusText})`;
+        const error = new Error(errorMessage);
+        throw error;
+      }
+      const body = await response.json();
+      setMovies(body.movies);
+    } catch (err) {
+      console.error(`Error in fetch: ${err.message}`);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const moviesListItems = movies.map((movieItem) => {
+    return <li key={movieItem.id}>{movieItem.title}</li>;
+  });
+
+  return (
+    <div>
+      <h1>Latest Movies</h1>
+      <ul>{moviesListItems}</ul>
+    </div>
+  );
+};
+
+export default MoviesList;
