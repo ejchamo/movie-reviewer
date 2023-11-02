866a44e20915bd58c7e0e78c4a119993a3f33b65
ejchamo <ejchamo@gmail.com>
2023-11-02 14:03:23 -0400

    make it dryy

--- client/src/components/MovieShow.js
+++ client/src/components/MovieShow.js
@@ -9,23 +9,6 @@ const MovieShow = (props) => {
     reviews: [],
   });
 
-  // const getMovie = async () => {
-  //   const movieId = props.match.params.id;
-  //   try {
-  //     const response = await fetch(`/api/v1/movies/${movieId}`);
-  //     if (!response.ok) {
-  //       const errorMessage = `${response.status} (${response.statusText})`;
-  //       const error = new Error(errorMessage);
-  //       throw error;
-  //     }
-  //     const body = await response.json();
-
-  //     setMovie(body.movie);
-  //   } catch (err) {
-  //     console.error(`Error in fetch: ${err.message}`);
-  //   }
-  // };
-
   const movieId = props.match.params.id;
 
   useEffect(() => {
