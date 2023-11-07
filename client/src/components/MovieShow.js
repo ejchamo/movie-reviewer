81a28a991c8874a5cd1fddbc417ccd04c3089a53
ejchamo <ejchamo@gmail.com>
2023-11-07 16:20:15 -0500

    more styling on movie show

--- client/src/components/MovieShow.js
+++ client/src/components/MovieShow.js
@@ -26,16 +26,18 @@ const MovieShow = (props) => {
 
   return (
     <>
-      <div className="grid-y medium-grid-frame">
-        <div className="medium-cell-block-container">
-          <div className="grid-x grid-padding-x">
-            <div className="movie-detail-container cell medium-6 medium-cell-block-y">
-              <h1>{movie.title}</h1>
-            </div>
+      <div className="grid-container">
+        <div className="grid-y medium-grid-frame">
+          <div className="medium-cell-block-container">
+            <div className="grid-x grid-padding-x">
+              <div className="movie-detail-container cell medium-6 medium-cell-block-y">
+                <h1>{movie.title}</h1>
+              </div>
 
-            <div className="reviews-list-container cell medium-6 medium-cell-block-y">
-              {newReviewFormLink}
-              <ReviewList user={props.user} movie={movie} setMovie={setMovie} />
+              <div className="reviews-list-container cell medium-6 medium-cell-block-y">
+                {newReviewFormLink}
+                <ReviewList user={props.user} movie={movie} setMovie={setMovie} />
+              </div>
             </div>
           </div>
         </div>
--- server/src/db/seeders/MovieSeeder.js
+++ server/src/db/seeders/MovieSeeder.js
@@ -12,6 +12,33 @@ class MovieSeeder {
       {
         title: "The Exorcist",
       },
+      {
+        title: "No Hard Feelings",
+      },
+      {
+        title: "The Marvels",
+      },
+      {
+        title: "Aladin",
+      },
+      {
+        title: "The Little Mermaid",
+      },
+      {
+        title: "Pocahontas",
+      },
+      {
+        title: "The Lion King",
+      },
+      {
+        title: "Iron Man",
+      },
+      {
+        title: "Spiderman",
+      },
+      {
+        title: "Ant Man",
+      },
     ];
 
     for (const movie of moviesData) {
