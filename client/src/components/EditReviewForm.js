0cf67c9f17de396075513c80f8bb3ecf94da6adf
Sky <43055693+skylfw@users.noreply.github.com>
2023-11-07 13:08:23 -0500

    Update EditReviewForm.js
    get rid of comments

--- client/src/components/EditReviewForm.js
+++ client/src/components/EditReviewForm.js
@@ -12,27 +12,16 @@ const EditReviewForm = (props) => {
 
   const editOnClick = async (event) => {
     event.preventDefault();
-    // implement editReview such that it returns the updatedReview when patched
-    const updateReview = await editReview(props.review.id, props.user.id, content);
-
+    const updateReview = await editReview(props.review.id, props.user.id, content)
     const reviewObject = {
       id: updateReview.updatedReview.id,
       content: updateReview.updatedReview.content,
       userId: updateReview.updatedReview.userId,
     };
-
-    // update the state that holds the reviews to replace the old review with the new one
-    // call setMovie (to change the list of reviews) with the updated review
-    // going to need to use toSpliced()
-
     const reviewList = props.movie.reviews;
     const index = reviewList.findIndex((review) => review.id === props.review.id);
-
     const splicedReview = reviewList.toSpliced(index, 1, reviewObject);
     props.setMovie({ title: props.movie.title, reviews: splicedReview });
-
-    // hide the edit review form
-    // we need to take in `setIsClicked` function as a prop, and call it here with false
     props.setIsClicked(false);
   };
 
