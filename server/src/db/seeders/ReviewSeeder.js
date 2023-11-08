ac4fc3db4f0376125f95944a31bb2ffd2246d962
ejchamo <ejchamo@gmail.com>
2023-11-08 15:21:09 -0500

    update review list css

--- server/src/db/seeders/ReviewSeeder.js
+++ server/src/db/seeders/ReviewSeeder.js
@@ -6,6 +6,24 @@ class ReviewSeeder {
     const fiveNights = await Movie.query().findOne({ title: "Five Nights at Freddy's" });
     const theExorcist = await Movie.query().findOne({ title: "The Exorcist" });
 
+    const noHardFeelings = await Movie.query().findOne({ title: "No Hard Feelings" });
+
+    const theMarvels = await Movie.query().findOne({ title: "The Marvels" });
+
+    const Aladin = await Movie.query().findOne({ title: "Aladin" });
+
+    const theLittleMermaid = await Movie.query().findOne({ title: "The Little Mermaid" });
+
+    const theLionKing = await Movie.query().findOne({ title: "Pocahontas" });
+
+    const ironMan = await Movie.query().findOne({ title: "Iron Man" });
+
+    const Spiderman = await Movie.query().findOne({ title: "Spiderman" });
+
+    const antMan = await Movie.query().findOne({ title: "Ant Man" });
+
+    const pocahontas = await Movie.query().findOne({ title: "Pocahontas" });
+
     const tyler = await User.query().findOne({ email: "tyler@gmail.com" });
     const sky = await User.query().findOne({ email: "skylfw@launch.com" });
     const ej = await User.query().findOne({ email: "ej@aol.com" });
@@ -64,6 +82,151 @@ class ReviewSeeder {
         userId: tyler.id,
         content: "disappointing",
         rating: 1,
+      },
+      {
+        movieId: noHardFeelings.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 5,
+      },
+      {
+        movieId: theMarvels.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 5,
+      },
+      {
+        movieId: Aladin.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 10,
+      },
+      {
+        movieId: theLittleMermaid.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 7,
+      },
+      {
+        movieId: pocahontas.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 10,
+      },
+      {
+        movieId: theLionKing.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 9,
+      },
+      {
+        movieId: ironMan.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 6,
+      },
+      {
+        movieId: Spiderman.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 10,
+      },
+      {
+        movieId: antMan.id,
+        userId: tyler.id,
+        content: "place holder review",
+        rating: 8,
+      },
+
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!!!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!!!!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!!!!!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!!!!!!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!!!!!!!!!!!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up!!!!!!!!!!!!!!!!!",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up$",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up$$",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up$$$",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up$$$$",
+        rating: 10,
+      },
+      {
+        movieId: dune2.id,
+        userId: ej.id,
+        content: "I'm review spamming this to bring its rating up$$$$$",
+        rating: 10,
       },
     ];
 
